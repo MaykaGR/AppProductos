f4c76ca7d7bdf56b803fce50e5e9faf2a761bb1a
MaykaGR <92048821+MaykaGR@users.noreply.github.com>
2022-11-18 17:49:15 +0100

    tyutu

--- src/main/kotlin/GestorCon.kt
+++ src/main/kotlin/GestorCon.kt
@@ -35,6 +35,49 @@ class GestorBDD private constructor() {
         println("[Desconexión de la base de datos]")
     }
 
+    fun mostrarTodo() {
+        if (con != null) {
+            val st = con!!.createStatement()
+            val rs = st.executeQuery("select * from productos;")
+            val rsmtd = rs.metaData
+            while (rs.next()) {
+                for (i in 1..rsmtd.columnCount) {
+                    println(rs.getString(i))
+                }
+            }
+        }
+    }
 
+    fun mostrarAlgo(name: String) {
+        if (con != null) {
+            val st = con!!.createStatement()
+            val rs = st.executeQuery("select * from productos where nombre = '$name'")
+            val rsmtd = rs.metaData
+            while (rs.next()) {
+                for (i in 1..rsmtd.columnCount) {
+                    println(rs.getString(i))
+                }
+            }
+        }
+    }
+    fun update(id: Int, precio: Int){
+        if(con!= null){
+            val st = con!!.createStatement()
+            val rs = st.executeUpdate("update productos set precio = $precio where id = $id")
+        }
+    }
 
+    fun delete(id:Int){
+        if (con!= null){
+            val st = con!!.createStatement()
+            val rs = st.executeUpdate("delete from productos where id = $id")
+        }
+    }
+
+    fun insert(id: Int,nombre:String,precio: Int){
+        if (con!= null){
+            val st = con!!.createStatement()
+            val rs = st.executeUpdate("insert into productos values ('$id','$nombre','$precio')")
+        }
+    }
 }
