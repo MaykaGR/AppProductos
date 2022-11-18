980f86661f14dc65d8380ccbe9ecbaee7d5a89bc
mariajesus16 <mjborrego@iesrafaelalberti.es>
2022-11-18 17:31:11 +0100

    wewe

--- src/main/kotlin/GestorCon.kt
+++ src/main/kotlin/GestorCon.kt
@@ -1,12 +1,40 @@
+import java.sql.Connection
+import java.sql.DriverManager
+
 class GestorBDD private constructor() {
-    companion object{
+    companion object {
         private var instance: GestorBDD? = null
 
-        fun getInstance(): GestorBDD{
-            if(instance==null){
+        fun getInstance(): GestorBDD {
+            if (instance == null) {
                 instance = GestorBDD()
             }
             return instance!!
         }
     }
+
+    private val url: String = "jdbc:mysql://localhost/"
+    private val bd: String = "bddproductos"
+    private val user: String = "root"
+    private val pass: String = ""
+
+    private var con: Connection? = null
+
+    fun conexion() {
+        if (con == null) {
+            Class.forName("com.mysql.cj.jdbc.Driver")
+            con = DriverManager.getConnection(url + bd, user, pass)
+            println("[Conexión realizada]")
+        } else {
+            println("[Conexión no realizada]")
+        }
+    }
+
+    fun desconexion() {
+        con?.close()
+        println("[Desconexión de la base de datos]")
+    }
+
+
+
 }
