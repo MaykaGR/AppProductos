68cd2981ec0d658346de649796dee63af981a59d
mariajesus16 <mjborrego@iesrafaelalberti.es>
2022-11-25 17:54:58 +0100

    i678789

--- src/main/kotlin/aplicacion/modelo/sentencias/sentenciasSQLapp.kt
+++ src/main/kotlin/aplicacion/modelo/sentencias/sentenciasSQLapp.kt
@@ -2,4 +2,5 @@ package aplicacion.modelo.sentencias
 
 object sentenciasSQLapp {
     val selectAllProducts: String = "Select * from productos where cantidad > 0;"
+    val selectAllClient: String = "Select * from clientes"
 }
